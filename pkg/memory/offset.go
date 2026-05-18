cffd8e8c4d2ee8af563b33ae27eb4aa50e039e6a
ooeygg <michaeljyenny@gmail.com>
2026-05-18 15:20:14 -0700

    Update memory offsets in calculateOffsets function

--- pkg/memory/offset.go
+++ pkg/memory/offset.go
@@ -26,55 +26,55 @@ type Offset struct {
 
 func calculateOffsets(_ *Process) Offset {
 	// UnitTable
-	unitTableOffset := uintptr(0x1EA73D0)
+	unitTableOffset := uintptr(0x1EB7460)
 
 	// UI
-	uiOffsetPtr := uintptr(0x1EB70CA)
+	uiOffsetPtr := uintptr(0x1EC715A)
 
 	// Hover
-	hoverOffset := uintptr(0x1DFB080)
+	hoverOffset := uintptr(0x1E0B110)
 
 	// Expansion
-	expOffset := uintptr(0x1DFA4E8)
+	expOffset := uintptr(0x1E0A578)
 
 	// Party members offset
-	rosterOffset := uintptr(0x1EBD6E8)
+	rosterOffset := uintptr(0x1ECD778)
 
 	// PanelManagerContainer
-	panelManagerContainerOffset := uintptr(0x1E11E40)
+	panelManagerContainerOffset := uintptr(0x1E21ED0)
 
 	// WidgetStates
-	WidgetStatesOffset := uintptr(0x1EDF700)
+	WidgetStatesOffset := uintptr(0x1EEF790)
 
 	// Waypoints
-	WaypointTableOffset := uintptr(0x1D59440)
+	WaypointTableOffset := uintptr(0x1D694D0)
 
 	// FPS
-	fpsOffset := uintptr(0x1D59414)
+	fpsOffset := uintptr(0x1D694A4)
 
 	// KeyBindings
-	keyBindingsOffset := uintptr(0x19D25B4)
+	keyBindingsOffset := uintptr(0x19E1434)
 
 	// KeyBindings Skills
-	keyBindingsSkillsOffset := uintptr(0x1DFB190)
+	keyBindingsSkillsOffset := uintptr(0x1E0B220)
 
 	// QuestInfo
-	questInfoOffset := uintptr(0x1EC3D58)
+	questInfoOffset := uintptr(0x1ED3DE8)
 
 	// Terror Zones
 	tzOffset := uintptr(0x25B1B80)
 
 	// Ping
-	pingOffset := uintptr(0x1DFA4E8)
+	pingOffset := uintptr(0x1E0A578)
 
 	// LegacyGraphics
-	legacyGfxOffset := uintptr(0x1EC3FC6)
+	legacyGfxOffset := uintptr(0x1ED4056)
 
 	// CharData
-	charDataOffset := uintptr(0x1DFE678)
+	charDataOffset := uintptr(0x1E0E708)
 
 	// Selected Char Name
-	selectedCharNameOffset := uintptr(0x1D50215)
+	selectedCharNameOffset := uintptr(0x1D60295)
 
 	// Last Game Name
 	lastGameNameOffset := uintptr(0x25FA4E0)
